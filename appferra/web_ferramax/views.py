d59468cc8be056d9c8761afae7642740f5d9ba62
LordDysK <127951851+LordDysK@users.noreply.github.com>
2024-05-29 20:40:36 -0400

    transbak full

--- appferra/web_ferramax/views.py
+++ appferra/web_ferramax/views.py
@@ -2,36 +2,53 @@ from django.shortcuts import render, redirect
 from django.conf import settings
 from .models import Payment
 from transbank.webpay.webpay_plus.transaction import Transaction
+import transbank.webpay.webpay_plus as webpay_plus
 
-
+webpay_plus.commerce_code = settings.TRANSBANK_COMMERCE_CODE
+webpay_plus.api_key = settings.TRANSBANK_API_KEY
+webpay_plus.environment = settings.TRANSBANK_ENVIRONMENT
 
 def prueba(request):
     return render(request, 'prueba.html')
 
+
 def start_payment(request):
     if request.method == 'POST':
-        amount = request.POST['amount']
-        order_id = request.POST['order_id']
+        amount = request.POST.get('amount')
+        order_id = request.POST.get('order_id')
+        session_id = request.session.session_key
 
-        transaction = Transaction(webpay_plus_default_commerce_code='your_commerce_code',
-                                  webpay_plus_default_api_key=settings.TRANSBANK_API_KEY)
+        # Asegúrate de que el session_id no esté vacío
+        if not session_id:
+            request.session.save()
+            session_id = request.session.session_key
+        print(f'Session ID: {session_id}')  # Depuración
+
+        transaction = Transaction()
         
-        response = transaction.create(order_id, session_id, amount, return_url='http://yourdomain/payments/callback/')
+        response = transaction.create(
+            buy_order=order_id,
+            session_id=session_id,
+            amount=amount,
+            return_url='http://127.0.0.1:8000/callback/'
+        )
+        print(response)  # Depuración
 
-        if response['status'] == 'INITIALIZED':
-            payment = Payment.objects.create(
-                order_id=order_id,
-                amount=amount,
-                token=response['token']
-            )
-            return redirect(response['url'] + '?token_ws=' + response['token'])
+        # Almacenar el pago en la base de datos
+        payment = Payment.objects.create(
+            order_id=order_id,
+            amount=amount,
+            token=response['token']
+        )
+        
+        # Redirigir a la URL proporcionada en la respuesta
+        return redirect(response['url'] + '?token_ws=' + response['token'])
 
     return render(request, 'start_payment.html')
 
 def payment_callback(request):
     token = request.GET.get('token_ws')
-    transaction = Transaction(webpay_plus_default_commerce_code='your_commerce_code',
-                              webpay_plus_default_api_key=settings.TRANSBANK_API_KEY)
+    transaction = Transaction()
     
     response = transaction.commit(token)
 
@@ -41,5 +58,3 @@ def payment_callback(request):
         payment.save()
 
     return render(request, 'payment_callback.html', {'response': response})
-
-# Create your views here.
